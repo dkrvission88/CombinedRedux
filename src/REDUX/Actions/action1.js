f94860cabc49f41354152b68b77091af29b08748
DKRVISSION <dkrvission88@gmail.com>
2024-02-27 10:41:28 +0530

    redux

---
+++ src/REDUX/Actions/action1.js
@@ -0,0 +1,177 @@
+import { REGISTER_FAILURE,REGISTER_SUCCESS,REGISTER_REQUEST,EDIT_FAILURE,EDIT_SUCCESS,EDIT_REQUEST,LOGIN_SUCCESS,LOGIN_FAILURE } from "../Constant/constant";
+
+import axios from "axios";
+
+// Supplier.jsx
+export const registerS = (formData) => {
+  return async (dispatch) => {
+    dispatch({ type: REGISTER_REQUEST });
+    try {
+      const response = await axios.post('http://localhost:8000/api/auth/contractor-register',{
+        method: 'PUT',
+
+        headers: {
+            'Content-Type': 'application/json',
+          },
+
+          body: JSON.stringify(formData),
+          
+      } );
+
+      console.log(response);
+
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: response.data
+      });
+    } catch (error) {
+      dispatch({
+        type: REGISTER_FAILURE,
+        payload: error.message
+      });
+    }
+  };
+};
+
+
+
+// Supplylogin.jsx
+export const login = (name, password) =>
+    
+    async dispatch => {
+  try {
+    const response = await axios.post('http://localhost:8000/api/auth/contractor-login', {
+      name,
+      password,
+
+      
+        method: 'POST',
+
+        headers: {
+            'Content-Type': 'application/json',
+          },
+
+          body: JSON.stringify(name, password),
+
+    });
+    dispatch({ type: LOGIN_SUCCESS,
+
+         payload: response.data
+        
+        });
+  } catch (error) {
+
+    dispatch({ type: LOGIN_FAILURE, 
+        
+        payload: error.message });
+  }
+};
+
+
+// EditProfileSupplier.jsx
+
+
+const editRequest = () => ({
+  type: EDIT_REQUEST,
+});
+
+const editSuccess = () => ({
+  type: EDIT_SUCCESS,
+});
+
+const editFailure = (error) => ({
+  type: EDIT_FAILURE,
+  error,
+});
+
+
+export const edit=(data)=>async(dispatch)=>{
+
+  try{
+      dispatch(editRequest())
+      const response=await axios.put('http://localhost:8000/api/auth/contractor-edit',{
+          method: 'PUT',
+
+          headers: {
+              'Content-Type': 'application/json',
+            },
+
+            body: JSON.stringify(data),
+
+      })
+
+      if (!response.ok) {
+          throw new Error('Failed to edit supplier');
+        }
+        dispatch(editSuccess());
+
+
+  }catch(error){
+      dispatch(editFailure(error.message))
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import * as constant from  "../Constant/constant";
+// import {CONTRACTOR_OR_SUPPLIRE_LOGIN} from  "../Constant/constant";
+
+
+
+
+// export const getSupplierL=(data)=>{
+//   return{
+//     type:CONTRACTOR_OR_SUPPLIRE_LOGIN,
+//     payload:data
+//   }
+// }
+
+
+
+// export const login = (username, password) => {
+//   return async (dispatch) => {
+//     dispatch({ type: constant.LOGIN_REQUEST });
+//     try {
+     
+//       const user = await fakeApiLogin(username, password);
+//       dispatch({ type: constant.LOGIN_SUCCESS, payload: { user } });
+//     } catch (error) {
+//       dispatch({ type: constant.LOGIN_FAILURE, payload: { error } });
+//     }
+//   };
+// };
+
+// export const logout = () => {
+//   return { type: constant.LOGOUT };
+// };
+
+
+// const fakeApiLogin = async (username, password) => {
+  
+//   await new Promise(resolve => setTimeout(resolve, 1000));
+  
+//   return { username }; 
+// };
---
+++ src/REDUX/REDUCERS/reducer1.js
@@ -0,0 +1,79 @@
+import { REGISTER_FAILURE,REGISTER_SUCCESS,REGISTER_REQUEST,LOGIN_SUCCESS,LOGIN_FAILURE,EDIT_REQUEST,EDIT_FAILURE,EDIT_SUCCESS } from "../Constant/constant";
+const initialState = {
+  loading: false,
+  error: null,
+  success: false,
+  data: null,
+
+  isAuthenticated: false,
+  user: null,
+ 
+};
+
+export const reduce01 = (state = initialState, action) => {
+  switch (action.type) {
+    case REGISTER_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+        success: false
+      };
+    case REGISTER_SUCCESS:
+      return {
+        ...state, 
+        loading: false,
+        success: true,
+        data: action.payload
+      };
+    case REGISTER_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+        success: false
+      };
+      case LOGIN_SUCCESS:
+        return {
+          ...state,
+          isAuthenticated: true,
+          user: action.payload,
+          error: null
+
+        //   console.log(action.payload)
+        };
+      case LOGIN_FAILURE:
+        return {
+          ...state,
+          isAuthenticated: false,
+          user: null,
+          error: action.payload
+
+        };
+
+        case EDIT_REQUEST:
+        return {
+          ...state,
+          loading: true,
+          error: null,
+        };
+      case EDIT_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          error: null,
+        };
+      case EDIT_FAILURE:
+        return {
+          ...state,
+          loading: false,
+          error: action.error,
+        };
+
+
+
+    default:
+      return state;
+  }
+};
+
